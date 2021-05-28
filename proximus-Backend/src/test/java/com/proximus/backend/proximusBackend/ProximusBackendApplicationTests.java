e95fa7e3d84c2044a84db7bc5bcd18f94f7323dd
2021-DEV1-061 <seydinassreddine.0509@gmail.com>
2021-05-28 11:33:32 +0200

    first commit

---
+++ proximus-Backend/src/test/java/com/proximus/backend/proximusBackend/ProximusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proximus.backend.proximusBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProximusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
